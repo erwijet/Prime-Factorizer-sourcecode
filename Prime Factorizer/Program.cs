046306f7a6ae481d3034fc0494535542375b8ced
Tmanbear <tyler.s.holewinski@gmail.com>
2017-08-29 20:53:54 -0400

    Add files via upload

---
+++ Prime Factorizer/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Factorizer
+{
+    class Program
+    {
+        /// <summary>
+        /// Returns the users input as a lowercase text string
+        /// </summary>
+        public static string requestInput
+        {
+            get
+            {
+                Console.Write("=> ");
+                string tmp = Console.ReadLine();
+                return tmp.ToLower();
+            }
+        }
+        /// <summary>
+        /// Data for indenting
+        /// </summary>
+        public static int indentInt = 0;
+        /// <summary>
+        /// returns the real indents based on the indentInt variable
+        /// </summary>
+        public static string indent
+        {
+            get
+            {
+                string tmp = "";
+                for (int i = 0; i < indentInt / 2; i++)
+                {
+                    tmp += " ";
+                }
+                return tmp;
+            }
+        }
+
+        /// <summary>
+        /// returns the last command the user entered
+        /// </summary>
+        public static string lastPrompt { get; set; } = "";
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //Get real user data
+                lastPrompt = requestInput;
+
+                //kill?
+                if (lastPrompt == "quit")
+                    break;
+                //Clear?
+                else if (lastPrompt == "clear")
+                    Console.Clear();
+
+                //Moving on...
+                else
+                {
+                    //The try will indicate if the number is able to be parsed or not, in other words, did the user enter a number?
+                    try
+                    {
+                        //Get the num equilivant of the input
+                        var num = Convert.ToInt32(lastPrompt);
+                        int length = 0;
+                        //Declare Encoder { Code for encoder EncodeNumber.cs }
+                        EncodeNumber en = new EncodeNumber();
+                        //Draw results
+                        #region draw
+                        foreach (int i in en.encodeNumber(Convert.ToInt32(lastPrompt)))
+                        {
+                            for (int e = 0; e < Console.BufferWidth - indent.Length - 1; e++)
+                            {
+                                Console.Write("_");
+                            }
+                            if (num == Convert.ToInt32(lastPrompt))
+                                Console.Write(" ");
+                            Console.Write(indent);
+                            Console.WriteLine("{0}|{1}", i.ToString(), num.ToString());
+                            num = num / i;
+                            indentInt += 4;
+                            Console.Write(indent);
+                            length++;
+                        }
+
+                        Console.WriteLine("\n");
+                        for (int r = 0; r < en.encodeNumber(Convert.ToInt32(lastPrompt)).Length; r++)
+                        {
+                            Console.Write(en.encodeNumber(Convert.ToInt32(lastPrompt))[r].ToString());
+                            if (r < length - 1)
+                                Console.Write(", ");
+                        }
+                        indentInt = 0;
+                        Console.WriteLine();
+                        #endregion
+                        //end draw results
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please provide a valid number to parse");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Prime Factorizer/class EncodeNumber.cs
@@ -0,0 +1,59 @@
+﻿//simple libs
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+/// <summary>
+/// Da Encoder
+/// </summary>
+class EncodeNumber
+{
+    //nil constructor
+    public EncodeNumber()
+    {
+    }
+
+    //this is where the magic happens
+    public int[] encodeNumber(int n)
+    {
+        if (n <= 1)
+            return null;
+        //int num = 1;  
+        int nnew = n;
+        List<int> arr = new List<int>();
+
+        while (nnew > 1)
+        {
+            //num = 1;  
+            for (int i = 2; i <= nnew; i++)
+            {
+                if (nnew % i == 0 && isPrime(i))
+                {
+                    arr.Add(i);
+                    //num = num * i;  
+                    nnew = nnew / i;
+                    break;
+                }
+
+            }
+            //nnew = nnew / num;  
+        }
+        return arr.ToArray();
+    }
+
+    //archived, use in update?
+    public Boolean isPrime(int x)
+    {
+        bool status = true;
+        for (int i = 2; i <= x / 2; i++)
+        {
+            if (x % i == 0)
+            {
+                status = false;
+                break;
+            }
+        }
+        return status;
+    }
+}
